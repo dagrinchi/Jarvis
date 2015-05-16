b4a3a87e616a871d10d2a697fb9b54c04b25a4a7
David Alméciga <wdavid@dagrinchi.com>
2015-05-16 11:37:59 -0500

    Initial Commit

---
+++ Jarvis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jarvis
+//
+//  Created by David Almeciga on 5/16/15.
+//  Copyright (c) 2015 dagrinchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jarvis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jarvis
+//
+//  Created by David Almeciga on 5/16/15.
+//  Copyright (c) 2015 dagrinchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
